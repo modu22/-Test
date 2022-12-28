ab1034a17098858a44e8346c491c60a3efee2331
modu22 <121487203+modu22@users.noreply.github.com>
2022-12-28 13:11:50 +0200

    Add files via upload

---
+++ gallery.php
@@ -0,0 +1,90 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> sudamrket</title>
+        <link rel="stylesheet" type="text/css" href="css\style">
+    </head>
+<body>
+    
+    <nav class="menu">
+
+     <ul>
+      <li><a href="http://localhost/market/radi.php">Home</a></li>    
+      <li><a href="http://localhost/market/gallery.php">Gallery</a></li> 
+      <li><a href="http://localhost/market/help.php">Help</a></li>
+      <li><a href="http://localhost/market/about.php">About</a></li>
+      <li><a href="http://localhost/market/COURS1.php">Sign in</a></li>
+     </ul>
+    
+ <form class="search-form">
+     
+     <input type="text" placeholder="search">
+
+     <button>search</button>
+
+ </form>
+ 
+     <img style="width: 100px;float: right;margin-right: 20px;" src="img/rg.png"  >
+ 
+     
+     </nav>
+<br>
+<div class="Contact">
+    <table style="margin-left:0px">
+        <tr>
+          <td style="background-color: white;margin-left: 30px;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px;" src="img/images-15.jpg"></td>
+
+          <td style="background-color:white;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-16.jpg"></td>
+
+            <td style="background-color:white;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-2.jpg"></td>
+
+            <td style="background-color:white;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/rm.jpg"></td>
+        </tr>
+
+        <tr>
+            <td style="background-color:white;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-49.jpg"></a></td>
+
+            <td style="background-color:white;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-39.jpg"></a></td>
+
+            <td style="background-color:white;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/img3.png"></td>
+
+            <td style="background-color:white;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/rt.jpg"></td>
+        </tr>
+
+        <tr>
+            <td style="background-color:white;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-27.jpg"></td>
+
+            <td style="background-color:white;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-36.jpg"></td>
+
+            <td style="background-color:white;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-15.jpg"></td>
+
+            <td style="background-color:white;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/images-16.jpg"></td>
+
+        </tr>
+
+        <tr>
+            <td style="background-color:white;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/img4.png"></td>
+
+            <td style="background-color:white;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/img5.jpg"></td>
+
+            <td style="background-color:white;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/img6.jpg"></td>
+
+            <td style="background-color:white;"><img style="width: 200px;height: 200px;padding: 20px 20px 20px 20px" src="img/img7.jpg"></td>
+        </tr>
+    </table>
+    
+</div>
+<br>
+    <div class="footer">
+      <table style="margin-left: 580px;">
+          <tr>
+              <td><a href="#"><h4 style="color: white;">call as   |</h4></a></td>
+              <td><a href="#"><h4 style="color:white;">politics   |</h4></a></td>
+              <td><a href="#"><h4 style="color:white;">privacy     </h4></a></td>
+          </tr>
+      </table>  
+      <p style="margin-left:603px;font-size: 10px;">جميع الحقوق محفوظة للدفعة 11 شبكات 2022</p>
+    </div>
+    
+</body>    
+</html>
